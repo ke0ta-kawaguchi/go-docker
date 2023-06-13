6e2bace8dc37e84afcfa5cdd1498d7b91ef01d6f
keita_kawaguchi <keikawagu@gmail.com>
2023-06-13 22:50:49 +0900

    add server and client

---
+++ app/server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server is running at localhost:8888")
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			panic(err)
+		}
+
+		go func() {
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+
+			if err != nil {
+				panic(err)
+			}
+
+			dump, err := httputil.DumpRequest(request, true)
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(dump)) // レスポンスを書き込む
+			response := http.Response{
+				StatusCode: 500,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body: io.NopCloser(strings.NewReader("Hello Go\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
+}
