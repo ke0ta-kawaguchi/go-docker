bb6ec9ca1c771e15923c635f6e887557875dd5b6
keita_kawaguchi <keikawagu@gmail.com>
2023-06-24 18:31:35 +0900

    add gzip

--- app/server/main.go
+++ app/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"net"
@@ -11,9 +13,60 @@ import (
 	"time"
 )
 
+func isGzipAcceptable(request *http.Request) bool {
+	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+}
+
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+
+	for {
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		response := http.Response{
+			StatusCode: 200,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header: make(http.Header),
+		}
+		if isGzipAcceptable(request) {
+			content := "Hello World (gzipped)\n"
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = io.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = io.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
+	}
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8888")
-
 	if err != nil {
 		panic(err)
 	}
@@ -21,48 +74,9 @@ func main() {
 
 	for {
 		conn, err := listener.Accept()
-
 		if err != nil {
 			panic(err)
 		}
-
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-
-			for {
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-
-				if err != nil {
-					neterr, ok := err.(net.Error)
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-
-				dump, err := httputil.DumpRequest(request, true)
-
-				if err != nil {
-					panic(err)
-				}
-
-				fmt.Println(string(dump))
-				content := "HelloWorld\n"
-
-				response := http.Response{
-					StatusCode: 200,
-					ProtoMajor: 1,
-					ProtoMinor: 1,
-					ContentLength: int64(len(content)),
-					Body: io.NopCloser(strings.NewReader(content)),
-				}
-				response.Write(conn)
-			}
-		}()
+		go processSession(conn)
 	}
 }
