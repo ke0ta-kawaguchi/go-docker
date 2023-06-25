fd0eadd62382821094ad591f5f1edb1741e268ba
keita_kawaguchi <keikawagu@gmail.com>
2023-06-25 09:24:28 +0900

    add chunk

--- app/server/main.go
+++ app/server/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"compress/gzip"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
-	"time"
 )
 
-func isGzipAcceptable(request *http.Request) bool {
-	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+var contents = []string{
+	"これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
+	"むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
+	"中山さまというおとのさまが、おられたそうです。",
+	"その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
+	"ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
+	"そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
 }
 
 func processSession(conn net.Conn) {
@@ -22,14 +24,9 @@ func processSession(conn net.Conn) {
 	defer conn.Close()
 
 	for {
-		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		request, err := http.ReadRequest(bufio.NewReader(conn))
 		if err != nil {
-			neterr, ok := err.(net.Error)
-			if ok && neterr.Timeout() {
-				fmt.Println("Timeout")
-				break
-			} else if err == io.EOF {
+			if err == io.EOF {
 				break
 			}
 			panic(err)
@@ -41,27 +38,17 @@ func processSession(conn net.Conn) {
 		}
 		fmt.Println(string(dump))
 
-		response := http.Response{
-			StatusCode: 200,
-			ProtoMajor: 1,
-			ProtoMinor: 1,
-			Header: make(http.Header),
+		fmt.Fprintf(conn, strings.Join([]string {
+			"HTTP/1.1 200 OK",
+			"Content-Type: text/plain",
+			"Transfer-Encoding: chunked",
+			"", "",
+		}, "\r\n"))
+		for _, content := range contents {
+			bytes := []byte(content)
+			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
 		}
-		if isGzipAcceptable(request) {
-			content := "Hello World (gzipped)\n"
-			var buffer bytes.Buffer
-			writer := gzip.NewWriter(&buffer)
-			io.WriteString(writer, content)
-			writer.Close()
-			response.Body = io.NopCloser(&buffer)
-			response.ContentLength = int64(buffer.Len())
-			response.Header.Set("Content-Encoding", "gzip")
-		} else {
-			content := "Hello World\n"
-			response.Body = io.NopCloser(strings.NewReader(content))
-			response.ContentLength = int64(len(content))
-		}
-		response.Write(conn)
+		fmt.Fprintf(conn, "0\r\n\r\n")
 	}
 }
 
