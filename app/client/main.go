584e8ce90946008a3e64cdc41ce1d848d630d97e
keita_kawaguchi <keikawagu@gmail.com>
2023-07-10 21:02:13 +0900

    add udp

--- app/client/main.go
+++ app/client/main.go
@@ -1,37 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"net/http"
-	"net/http/httputil"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8888")
-
+	conn, err := net.Dial("udp4", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
+	defer conn.Close()
+	fmt.Println("Sending to server")
 
-	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
+	_, err = conn.Write([]byte("Hello from Client"))
 	if err != nil {
 		panic(err)
 	}
-
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
 	if err != nil {
 		panic(err)
 	}
-
-	dump, err := httputil.DumpResponse(response, true)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(dump))
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
 }
--- app/server/main.go
+++ app/server/main.go
@@ -1,53 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"net"
-	"net/http"
-	"net/http/httputil"
-	"strings"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8888")
-
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp", "localhost:8888")
+	
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Server is running at localhost:8888")
+	defer conn.Close()
 
+	buffer := make([]byte, 1500)
+	
 	for {
-		conn, err := listener.Accept()
-
+		length, remoteAddress, err := conn.ReadFrom(buffer)
 		if err != nil {
 			panic(err)
 		}
-
-		go func() {
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-
-			if err != nil {
-				panic(err)
-			}
-
-			dump, err := httputil.DumpRequest(request, true)
-
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(string(dump)) // レスポンスを書き込む
-			response := http.Response{
-				StatusCode: 500,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body: io.NopCloser(strings.NewReader("Hello Go\n")),
-			}
-			response.Write(conn)
-			conn.Close()
-		}()
+		fmt.Printf("Received from %v: %v\n", remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
